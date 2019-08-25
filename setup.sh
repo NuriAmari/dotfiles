aacde8ca5d0cbc0483705dc1280b25baacf32b58
Nuri Amari <nuriamari@Nuris-MacBook-Pro.local>
2019-08-25 17:43:45 -0600

    Add yabai & skhdrc example configs

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 
 DOTFILES_DIR=$(pwd)
 
-ln -s -f $DOTFILES_DIR/.vimrc ~/.vimrc 
+ln -s -f $DOTFILES_DIR/.vimrc ~/.vimrc
 ln -s -f $DOTFILES_DIR/.bashrc ~/.bashrc
+ln -s -f $DOTFILES_DIR/.yabairc ~/.yabairc
+ln -s -f $DOTFILES_DIR/.skhdrc ~/.skhdrc
