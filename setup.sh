cdda89af03348a623ecf70ac3a16c837a19b70b9
Nuri Amari <nuri.amari99@gmail.com>
2019-08-26 20:33:57 -0400

    Add python3 install to setup

--- setup.sh
+++ setup.sh
@@ -12,3 +12,8 @@ ln -s -f $DOTFILES_DIR/.bash_profile ~/.bash_profile
 if [ ! $(command -v brew) ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+# install python3 if not already installed
+if [ ! $(command -v python3) ]; then
+    brew install python
+fi
